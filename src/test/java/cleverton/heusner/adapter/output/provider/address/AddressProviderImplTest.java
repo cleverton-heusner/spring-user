1e4dde0f85ddc61739f97119f97b6f42c33e8ad9
cleverton-heusner <heusner.cleverton@gmail.com>
2024-11-09 20:50:48 -0300

    Test fixed.

--- src/test/java/cleverton/heusner/adapter/output/provider/address/AddressProviderImplTest.java
+++ src/test/java/cleverton/heusner/adapter/output/provider/address/AddressProviderImplTest.java
@@ -2,6 +2,7 @@ package cleverton.heusner.adapter.output.provider.address;
 
 import cleverton.heusner.adapter.output.mapper.AddressResponseMapper;
 import cleverton.heusner.adapter.output.response.AddressResponse;
+import cleverton.heusner.domain.exception.ResourceNotFoundException;
 import cleverton.heusner.domain.exception.UnavailableResourceException;
 import cleverton.heusner.domain.model.Address;
 import cleverton.heusner.port.shared.LoggerComponent;
@@ -17,12 +18,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
+import static cleverton.heusner.adapter.input.constant.business.AddressBusinessErrorMessage.NOT_FOUND_ADDRESS_PROVIDER_MESSAGE;
 import static cleverton.heusner.adapter.input.constant.business.AddressBusinessErrorMessage.UNAVAILABLE_ADDRESS_PROVIDER_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -98,18 +102,18 @@ public class AddressProviderImplTest {
     void when_addressProviderIsUnavailable_then_throwUnavailableResourceException() {
 
         // Arrange
+        final var internalErrorException = new HttpClientErrorException(HttpStatusCode.valueOf(500));
         final String expectedUnavailableAddressProviderMessage = "Address provider service is currently unavailable";
         final String zipCode = "12345678";
         final Address incompleteAddress = Instancio.of(Address.class)
                 .set(Select.field(Address::getZipCode), zipCode)
                 .set(Select.field(Address::isComplete), false)
                 .create();
-        final ResponseEntity<AddressResponse> responseEntity = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
         when(messageComponent.getMessage(UNAVAILABLE_ADDRESS_PROVIDER_MESSAGE)).thenReturn(
                 expectedUnavailableAddressProviderMessage
         );
-        when(restTemplate.getForEntity(uri, AddressResponse.class)).thenReturn(responseEntity);
+        when(restTemplate.getForEntity(uri, AddressResponse.class)).thenThrow(internalErrorException);
 
         // Act & Assert
         assertThatThrownBy(() -> addressProvider.getAddressByZipCode(incompleteAddress))
@@ -117,6 +121,33 @@ public class AddressProviderImplTest {
                 .hasMessage(expectedUnavailableAddressProviderMessage);
 
         verify(messageComponent).getMessage(UNAVAILABLE_ADDRESS_PROVIDER_MESSAGE);
+        verify(restTemplate).getForEntity(any(URI.class), eq(AddressResponse.class));
+        verifyNoInteractions(addressResponseMapper);
+    }
+
+    @Test
+    void when_cepNotFoundInAddressProvider_then_throwResourceNotFoundException() {
+
+        // Arrange
+        final var resourceNotFoundException = new HttpClientErrorException(HttpStatusCode.valueOf(404));
+        final String expectedNotFoundCepMessage = "CEP not Found";
+        final String zipCode = "12345678";
+        final Address incompleteAddress = Instancio.of(Address.class)
+                .set(Select.field(Address::getZipCode), zipCode)
+                .set(Select.field(Address::isComplete), false)
+                .create();
+
+        when(messageComponent.getMessage(NOT_FOUND_ADDRESS_PROVIDER_MESSAGE)).thenReturn(
+                expectedNotFoundCepMessage
+        );
+        when(restTemplate.getForEntity(uri, AddressResponse.class)).thenThrow(resourceNotFoundException);
+
+        // Act & Assert
+        assertThatThrownBy(() -> addressProvider.getAddressByZipCode(incompleteAddress))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage(expectedNotFoundCepMessage);
+
+        verify(messageComponent).getMessage(NOT_FOUND_ADDRESS_PROVIDER_MESSAGE);
         verify(restTemplate).getForEntity(any(URI.class), eq(AddressResponse.class));
         verifyNoInteractions(addressResponseMapper);
     }
